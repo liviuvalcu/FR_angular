041bd08e41e939bcdcc0b3206d65a7e43262827e
Liviu Valcu <liviu.valcu@yahoo.com>
2023-07-21 12:42:33 +0300

    fix json

--- src/app/interceptors/AuthInterceptor.ts
+++ src/app/interceptors/AuthInterceptor.ts
@@ -15,10 +15,12 @@ export class AuthInterceptor implements HttpInterceptor{
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         let authReq = req;
-        const token = this.storageService.getUser().token;
-        if (token != null) {
-            authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+        const user = this.storageService.getUser();
+
+        if (user != null) {
+            authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + user.token) });
         }
+
         return next.handle(authReq);
     }
 }
